1555d45a617cc7f74cf57e2d71a962bdf4d5e08a
npolyak <nick.polyak@verizon.net>
2018-03-16 15:31:53 -0400

    created the test

---
+++ DefaultWrappersTest/PersonDataVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultWrappersTest
+{
+    // IPersonDataVM interface
+    public interface IPersonDataVM
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+        string FullName { get; }
+    }
+
+    // IPersonDataVM implementation
+    public class PersonDataVM : IPersonDataVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // FullName is a calculated property
+        public string FullName =>
+            FirstName + " " + LastName;
+    }
+
+
+    // wrapper interface. 
+    public interface IPersonDataWrapper
+    {
+        PersonDataVM ThePersonWrapper { get; }
+    }
+}
--- DefaultWrappersTest/Program.cs
+++ DefaultWrappersTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NP.Roxy;
+using NP.Utilities.Behaviors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,40 @@ using System.Threading.Tasks;
 
 namespace DefaultWrappersTest
 {
+    public interface ISelectablePersonVM : IPersonDataVM, ISelectableItem<ISelectablePersonVM>
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Core.SetWrapperType<IPersonDataVM, IPersonDataWrapper>();
+            Core.SetWrapperType(typeof(ISelectableItem<>), typeof(ISelectableItemWrapper<>));
+
+            // create the instance of a class that implements ISelectablePersonVM
+            // using automatic wrapper mappings defined above
+            ISelectablePersonVM personVM = 
+                Core.CreateInstanceOfGeneratedType<ISelectablePersonVM>();
+
+            personVM.FirstName = "Joe";
+            personVM.LastName = "Doe";
+
+            // make sure that FullName is "Joe Doe"
+            Console.WriteLine($"FullName is '{personVM.FullName}'");
+
+            // make sure that the IsSelectedChanged event fires. 
+            personVM.IsSelectedChanged += PersonVM_IsSelectedChanged;
+
+            // when IsSelected changes.
+            personVM.IsSelected = true;
+        }
+
+        private static void PersonVM_IsSelectedChanged(ISelectableItem<ISelectablePersonVM> obj)
+        {
+            // make sure that the IsSelectedChanged event fires. 
+            Console.WriteLine("Is Selected Changed");
         }
     }
 }
